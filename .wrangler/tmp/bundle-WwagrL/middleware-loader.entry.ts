1d361e761c74d50ee219e4358811de795caf6694
jon <jon_searle@outlook.com>
2026-04-12 13:57:18 +0100

    upgrade to Expo SDK 55. must use a development build

--- .wrangler/tmp/bundle-WwagrL/middleware-loader.entry.ts
+++ .wrangler/tmp/bundle-WwagrL/middleware-loader.entry.ts
@@ -3,12 +3,12 @@
 // export dynamically through wrangler, or we can potentially let users directly
 // add them as a sort of "plugin" system.
 
-import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "C:\\Users\\jon_s\\source\\repos\\blind-taster\\.wrangler\\tmp\\bundle-xu1tVp\\middleware-insertion-facade.js";
+import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "C:\\Users\\jon_s\\source\\repos\\blind-taster\\.wrangler\\tmp\\bundle-WwagrL\\middleware-insertion-facade.js";
 import { __facade_invoke__, __facade_register__, Dispatcher } from "C:\\Users\\jon_s\\source\\repos\\blind-taster\\node_modules\\wrangler\\templates\\middleware\\common.ts";
-import type { WorkerEntrypointConstructor } from "C:\\Users\\jon_s\\source\\repos\\blind-taster\\.wrangler\\tmp\\bundle-xu1tVp\\middleware-insertion-facade.js";
+import type { WorkerEntrypointConstructor } from "C:\\Users\\jon_s\\source\\repos\\blind-taster\\.wrangler\\tmp\\bundle-WwagrL\\middleware-insertion-facade.js";
 
 // Preserve all the exports from the worker
-export * from "C:\\Users\\jon_s\\source\\repos\\blind-taster\\.wrangler\\tmp\\bundle-xu1tVp\\middleware-insertion-facade.js";
+export * from "C:\\Users\\jon_s\\source\\repos\\blind-taster\\.wrangler\\tmp\\bundle-WwagrL\\middleware-insertion-facade.js";
 
 class __Facade_ScheduledController__ implements ScheduledController {
 	readonly #noRetry: ScheduledController["noRetry"];
